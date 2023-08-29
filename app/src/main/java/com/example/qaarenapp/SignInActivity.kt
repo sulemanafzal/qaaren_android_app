9d4900b6beb186cfb691a51419cab7e13fefd7d5
SulemanAfzal <sulemanafzalbkuc1998@gmail.com>
2023-08-29 13:30:38 +0500

    Sign in and signup designcomplete

--- app/src/main/java/com/example/qaarenapp/SignInActivity.kt
+++ app/src/main/java/com/example/qaarenapp/SignInActivity.kt
@@ -1,11 +1,21 @@
 package com.example.qaarenapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.qaarenapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    lateinit var binding:ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    binding.tvSkipLogin.setOnClickListener {
+        val intent = Intent(this@SignInActivity, MainActivity::class.java)
+        startActivity(intent)
+    }
     }
 }
--- app/src/main/java/com/example/qaarenapp/SignUpActivity.kt
+++ app/src/main/java/com/example/qaarenapp/SignUpActivity.kt
@@ -1,11 +1,23 @@
 package com.example.qaarenapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.qaarenapp.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this@SignUpActivity, SignInActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 }
